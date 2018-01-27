fb6f4175295df4036000846e6c23c16ebd145485
Min Kim <min.c.kim@gmail.com>
2018-01-26 18:01:08 -0600

    Cleaned up some code

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 #import libraries
 from flask import Flask, render_template, redirect
 import pymongo
-
+import Mission_to_Mars
 #create flask app
 app = Flask(__name__)
 
 #create pymongo client
-client = pymongo.Mongoclient()
+client = pymongo.MongoClient()
 db = client.mars_db
+collection = db.mars
+
+@app.route("/")
+def home():
+    data = list(db.collection.find())
+    print(data)
+    return render_template("index.html", data = data)
+
+@app.route("/scrape")
+def scrape_data():
+    data = Mission_to_Mars.scrape()
+    db.collection.insert_one(data)
+    return "Scraped"
+
+if __name__ == "__main__":
+    app.run(debug=True)
